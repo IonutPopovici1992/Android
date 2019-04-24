6c73d9ac4dc2e79592e69945204d125f674436e7
John <ionut_popovici92@yahoo.com>
2019-04-24 23:27:55 +0300

    CS 193A Lecture 04: Files (Winter 2017)

--- DictionaryLists/app/src/main/java/com/example/dictionarylists/MainActivity.java
+++ DictionaryLists/app/src/main/java/com/example/dictionarylists/MainActivity.java
@@ -9,19 +9,50 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Random;
+import java.util.Scanner;
 import java.util.TreeMap;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String[] WORDS = {
-            "mediocre", "UC Berkeley",
-            "underachiver", "Stanford A- student",
-            "jerk", "Marty Stepp"
-    };
-
+    // a dictionary of {word -> definition} pairs for lookup
     private Map<String, String> dictionary;
+    private List<String> words;
+
+    private void readFileData() {
+        Scanner scanner = new Scanner(getResources().openRawResource(R.raw.grewords));
+        while (scanner.hasNextLine()) {
+            // "abate -> to lessen to subside"
+            String line = scanner.nextLine();
+            String[] parts = line.split("\t");
+            dictionary.put(parts[0], parts[1]);
+            words.add(parts[0]);
+        }
+    }
+
+    // pick the word;
+    // pick 5 random defns for that word (1 is correct)
+    // show all of that on screen
+    private void chooseWords() {
+        Random randomNumber = new Random();
+        int randomIndex = randomNumber.nextInt(words.size());
+        String theWord = words.get(randomIndex);
+        String theDefinition = dictionary.get(theWord);
+
+        // pick 4 other (wrong) definitions at random
+        List<String> defns = new ArrayList<>(dictionary.values());
+        defns.remove(theDefinition);
+        Collections.shuffle(defns);
+        defns = defns.subList(0, 4);
+        defns.add(theDefinition);
+        Collections.shuffle(defns);
+
+        // weird stuff ...
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,10 +60,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         dictionary = new HashMap<>();
-        for (int i = 0; i < WORDS.length; i += 2) {
-            dictionary.put(WORDS[i], WORDS[i + 1]);
-        }
+        words = new ArrayList<>();
 
+        readFileData();
+        chooseWords();
+
+        // put the dictionary words into an Adapter so they can be seen in List
         ListView list = findViewById(R.id.word_list);
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(
@@ -43,14 +76,13 @@ public class MainActivity extends AppCompatActivity {
 
         list.setAdapter(adapter);
 
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        // this is the code that should run when the user taps items in the list
+        list.setOnItemClickListener( (parent, view, position, id) -> {
                 String word = parent.getItemAtPosition(position).toString();
                 String definition = dictionary.get(word);
                 Toast.makeText(MainActivity.this, definition, Toast.LENGTH_SHORT).show();
             }
-        });
+        );
     }
 
 }
